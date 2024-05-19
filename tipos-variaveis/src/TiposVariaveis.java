607f15483eed386dcb0284508cf7ae185038805a
brunaszarin <brunaszarin@gmail.com>
2024-05-19 17:25:45 -0300

    Update tipos de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+
+        /*A estrutura padrão para se declarar uma variável sempre é:
+         *
+         *  <Tipo> <nomeVariavel> <atribuicaoDeValorOpcional>
+         * 
+         * Exemplos abaixo:
+         * 
+         * int idade; //Tipo "int", nome "idade", com nenhum valor atribuído. 
+         * int anoFabricacao = 2021; //tipo "int", nome "anoFabricacao", com * * valor 2021.
+         * double salarioMinimo = 2.500; //tipo "double", nome "salarioMinimo", * valor 2.500.
+         */
+        byte idade = 123;
+		short ano = 2021;
+		int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
+		long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
+		float pi = 3.14F;
+		double salario = 1275.33;
     }
 }
