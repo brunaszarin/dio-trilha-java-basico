d04ded682546ed24141b23d78753a1d12c6cb348
brunaszarin <brunaszarin@gmail.com>
2024-05-19 17:39:56 -0300

    update no curso

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -12,12 +12,19 @@ public class TiposVariaveis {
          * int anoFabricacao = 2021; //tipo "int", nome "anoFabricacao", com * * valor 2021.
          * double salarioMinimo = 2.500; //tipo "double", nome "salarioMinimo", * valor 2.500.
          */
+
+
         byte idade = 123;
+
 		short ano = 2021;
-		int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
+
+		int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo.
+
 		long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo, e precisa terminar com L.
+
 		float pi = 3.14F; // precisa sempre terminar com F o tipo float.
-		double salario = 1275.33;
+		
+        double salario = 1275.33;
 
         int numero = 1;
 
