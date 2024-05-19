061542b114f08845dbac821a67b259be38012d0a
brunaszarin <brunaszarin@gmail.com>
2024-05-19 17:27:20 -0300

    Update tipos de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -15,8 +15,8 @@ public class TiposVariaveis {
         byte idade = 123;
 		short ano = 2021;
 		int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
-		long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
-		float pi = 3.14F;
+		long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo, e precisa terminar com L.
+		float pi = 3.14F; // precisa sempre terminar com F o tipo float.
 		double salario = 1275.33;
     }
 }
