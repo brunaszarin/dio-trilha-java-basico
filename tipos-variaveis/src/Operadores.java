a35f98a4691ecf19a6ef16963952939771b1ae11
brunaszarin <brunaszarin@gmail.com>
2024-05-19 18:23:28 -0300

    Updated Operadores

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -2,13 +2,25 @@ public class Operadores {
 
    public static void main(String[] args) {
     
+        //A atribuição de valor se dá pelo símbolo de igualdade "="
+    
     //classe Operadores.java
-    String nome = "GLEYSON";
+    String nome = "BRUNA";
     int idade = 22;
     double peso = 68.5;
     char sexo = 'M';
     boolean doadorOrgao = false;
     Date dataNascimento = new Date();
+
+        //O operador de adição (+), quando utilizado em variáveis do tipo texto, realizará a “concatenação de textos”.
+
+    //classe Operadores.java
+    double soma = 10.5 + 15.7;
+    int subtração = 113 - 25;
+    int multiplicacao = 20 * 7;
+    int divisao = 15 / 3;
+    int modulo = 18 % 3;
+    double resultado = (10 * 7) + (20/4);
 
    } 
 }
