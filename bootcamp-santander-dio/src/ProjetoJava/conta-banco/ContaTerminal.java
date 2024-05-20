ec979757bbd2999ce5c12f498a58e7f0c4fa41dc
brunaszarin <brunaszarin@gmail.com>
2024-05-19 23:57:06 -0300

    Project Finalized

--- bootcamp-santander-dio/src/ProjetoJava/conta-banco/ContaTerminal.java
+++ bootcamp-santander-dio/src/ProjetoJava/conta-banco/ContaTerminal.java
@@ -1,5 +1,41 @@
 package ProjetoJava;
 
-public class conta-banco {
-    
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+    public static void main(String[] args) {
+        // Declaração das variáveis
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        // Instância do Scanner para leitura dos dados
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitação dos dados ao usuário
+        System.out.println("Por favor, digite o número da Conta:");
+        numero = scanner.nextInt();
+        scanner.nextLine(); // Consumir a quebra de linha
+
+        System.out.println("Por favor, digite o número da Agência:");
+        agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o nome do Cliente:");
+        nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo da Conta:");
+        saldo = scanner.nextDouble();
+
+        // Exibição da mensagem final
+        String mensagem = "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +
+                ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.";
+        
+        System.out.println(mensagem);
+
+        // Fechar o scanner
+        scanner.close();
+    }
 }
+
