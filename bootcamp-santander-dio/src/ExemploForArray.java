b71f1f4ff56db037bd6c104d6cb0f86abc16ef23
brunaszarin <brunaszarin@gmail.com>
2024-06-06 23:57:52 -0300

    Create ExemploForArray.java

---
+++ bootcamp-santander-dio/src/ExemploForArray.java
@@ -0,0 +1,9 @@
+
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos [] = {"FELIPE", "JOMAS", "JULIA", "MARCOS" };
+
+        for (int x=0; x < alunos.length; x++) {
+
+            System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+}
