f29c0d866337291ffcffc24668762998a236c877
brunaszarin <brunaszarin@gmail.com>
2024-06-14 20:55:13 -0300

    Corrections

---
+++ bootcamp-santander-dio/src/ExemploExcecao.java
@@ -0,0 +1,7 @@
+
+public class ExemploExcecao{
+    public static void main(String[] args) {
+        Number valor = Double.valueOf("a1.75");
+        System.out.println("valor");
+    }
+}
--- bootcamp-santander-dio/src/ProjetoJava/conta-banco/ExemploExcecao.java
+++
@@ -1,6 +0,0 @@
-public enum ExemploExcecao {
-    public static Void main(String[] args){
-        Number valor = double.valueOf("a1.75");
-        System.out.println(valor);
-    }
-}
