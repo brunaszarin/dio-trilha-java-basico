36fba6c2eb542b3c24d33a6218b88217d03d1da6
brunaszarin <brunaszarin@gmail.com>
2024-06-14 23:51:51 -0300

    Update ExemploExcecao.java

--- bootcamp-santander-dio/src/ExemploExcecao.java
+++ bootcamp-santander-dio/src/ExemploExcecao.java
@@ -1,7 +1,13 @@
 
 public class ExemploExcecao{
     public static void main(String[] args) {
-        Number valor = Double.valueOf("a1.75");
-        System.out.println("valor");
-    }
+        Number valor;
+        try {
+            valor = NumberFormat.getInstance().parse("a1.75");
+            System.out.println(valor);
+    }   catch (ParseException e) {
+          e.printStackTrace();        
+    
+    } 
+}    
 }
