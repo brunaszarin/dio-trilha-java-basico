de91ce73c6c6f9310df3b723e96986725b334ec5
brunaszarin <brunaszarin@gmail.com>
2024-06-25 00:56:25 -0300

    Update contador.java

--- bootcamp-santander-dio/src/ProjetoJava/contador/contador.java
+++ bootcamp-santander-dio/src/ProjetoJava/contador/contador.java
@@ -1,5 +1,28 @@
 package ProjetoJava.contador;
 
+import java.util.Scanner;
+
 public class contador {
-    
+    public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in);
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.??;
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.??;
+		
+		try {
+			//chamando o método contendo a lógica de contagem
+			contar(parametroUm, parametroDois);
+		
+		}catch (? exception) {
+			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+		}
+		
+	}
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+		
+		int contagem = parametroDois - parametroUm;
+		//realizar o for para imprimir os números com base na variável contagem
+	}
 }
